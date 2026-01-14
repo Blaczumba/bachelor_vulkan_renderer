4df5e05005ff6e6fa665ea805eaa8166e7595ab4
Bejzak <ejzakblazej@gmail.com>
2026-01-14 20:41:16 +0100

    Shaders scripts

--- shaders/compile_shaders.bat
+++ shaders/compile_shaders.bat
@@ -11,15 +11,15 @@ glslc.exe -I "%SCRIPT_DIR%\bindless.glsl" -fshader-stage=fragment "%SCRIPT_DIR%\
 glslc.exe -fshader-stage=vertex "%SCRIPT_DIR%\shadow.vert.glsl" -O -o "%SCRIPT_DIR%\shadow.vert.spv"
 glslc.exe -fshader-stage=fragment "%SCRIPT_DIR%\shadow.frag.glsl" -O -o "%SCRIPT_DIR%\shadow.frag.spv"
 
-glslc.exe -I "%SCRIPT_DIR%\bindless.glsl" -I "%SCRIPT_DIR%\16bit_push_constants.glsl" -fshader-stage=vertex "%SCRIPT_DIR%\env_mapping_phong.vert.glsl" -O -o "%SCRIPT_DIR%\env_mapping_phong.vert.spv"
-glslc.exe -I "%SCRIPT_DIR%\bindless.glsl" -I "%SCRIPT_DIR%\16bit_push_constants.glsl" -fshader-stage=frag "%SCRIPT_DIR%\env_mapping_phong.frag.glsl" -O -o "%SCRIPT_DIR%\env_mapping_phong.frag.spv"
+glslc.exe -I "%SCRIPT_DIR%\bindless.glsl" -I "%SCRIPT_DIR%\32bit_push_constants.glsl" -I "%SCRIPT_DIR%\16bit_push_constants.glsl" -fshader-stage=vertex "%SCRIPT_DIR%\env_mapping_phong.vert.glsl" -O -o "%SCRIPT_DIR%\env_mapping_phong.vert.spv"
+glslc.exe -I "%SCRIPT_DIR%\bindless.glsl" -I "%SCRIPT_DIR%\32bit_push_constants.glsl" -I "%SCRIPT_DIR%\16bit_push_constants.glsl" -fshader-stage=frag "%SCRIPT_DIR%\env_mapping_phong.frag.glsl" -O -o "%SCRIPT_DIR%\env_mapping_phong.frag.spv"
 
-glslc.exe -I "%SCRIPT_DIR%\bindless.glsl" -I "%SCRIPT_DIR%\16bit_push_constants.glsl" -fshader-stage=vertex "%SCRIPT_DIR%\pbr_env_mapping.vert.glsl" -O -o "%SCRIPT_DIR%\pbr_env_mapping.vert.spv"
+glslc.exe -I "%SCRIPT_DIR%\bindless.glsl" -I "%SCRIPT_DIR%\32bit_push_constants.glsl" -I "%SCRIPT_DIR%\16bit_push_constants.glsl" -fshader-stage=vertex "%SCRIPT_DIR%\pbr_env_mapping.vert.glsl" -O -o "%SCRIPT_DIR%\pbr_env_mapping.vert.spv"
 
-glslc.exe -I "%SCRIPT_DIR%\bindless.glsl" -I "%SCRIPT_DIR%\16bit_push_constants.glsl" -fshader-stage=vertex "%SCRIPT_DIR%\shader_pbr.vert.glsl" -O -o "%SCRIPT_DIR%\shader_pbr.vert.spv"
+glslc.exe -I "%SCRIPT_DIR%\bindless.glsl" -I "%SCRIPT_DIR%\32bit_push_constants.glsl" -I "%SCRIPT_DIR%\16bit_push_constants.glsl" -fshader-stage=vertex "%SCRIPT_DIR%\shader_pbr.vert.glsl" -O -o "%SCRIPT_DIR%\shader_pbr.vert.spv"
 glslc.exe -fshader-stage=vertex "%SCRIPT_DIR%\shader_pbr_tesselation.vert.glsl" -O -o "%SCRIPT_DIR%\shader_pbr_tesselation.vert.spv"
 glslc.exe -fshader-stage=tesscontrol "%SCRIPT_DIR%\shader_pbr_tesselation.tsc.glsl" -O -o "%SCRIPT_DIR%\shader_pbr_tesselation.tsc.spv"
 glslc.exe -fshader-stage=tesseval "%SCRIPT_DIR%\shader_pbr_tesselation.tse.glsl" -O -o "%SCRIPT_DIR%\shader_pbr_tesselation.tse.spv"
 glslc.exe -fshader-stage=frag "%SCRIPT_DIR%\shader_pbr_tesselation.frag.glsl" -O -o "%SCRIPT_DIR%\shader_pbr_tesselation.frag.spv"
 
-glslc.exe -I "%SCRIPT_DIR%\bindless.glsl" -I "%SCRIPT_DIR%\16bit_push_constants.glsl" -fshader-stage=frag "%SCRIPT_DIR%\shader_pbr.frag.glsl" -O -o "%SCRIPT_DIR%\shader_pbr.frag.spv"
+glslc.exe -I "%SCRIPT_DIR%\bindless.glsl" -I "%SCRIPT_DIR%\32bit_push_constants.glsl" -I "%SCRIPT_DIR%\16bit_push_constants.glsl" -fshader-stage=frag "%SCRIPT_DIR%\shader_pbr.frag.glsl" -O -o "%SCRIPT_DIR%\shader_pbr.frag.spv"
